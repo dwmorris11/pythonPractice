51dfbd04ae80a928256db87b1167afbceb248468
Dustin Morris <dustinnuc@gmail.com>
2020-08-20 12:05:47 -0700

    type, floats

--- basics.py
+++ basics.py
@@ -2,3 +2,16 @@ import datetime
 print("The date and time is")
 print(datetime.datetime.now())
 print("The date and time is", datetime.datetime.now())
+
+mynow = datetime.datetime.now()
+print(mynow)
+
+x = 10
+y = "10"
+
+sum1 = x + x
+sum2 = y + y
+z = 10.1
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
